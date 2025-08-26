696ad271298acac9b0797ffea99245efd4428296
Ruben <rub3n888@gmail.com>
2025-08-26 14:20:36 +0200

    Ejercicios 2.1-2.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,85 +1,49 @@
-import { useState } from "react";
+const Course = ({ course }) => {
+  const items = course.parts.map((part) => (
+    <li key={part.id}>
+      {part.name} {part.exercises}
+    </li>
+  ));
+  const suma = course.parts.reduce((total, part) => total + part.exercises,0)
 
-const StatisticLine = ({ value, text }) => (
-  <p>
-    {value} {text}
-  </p>
-);
-
-const ButtonCounter = ({ onClick, text }) => (
-  <button onClick={onClick}>{text}</button>
-);
-
-const AllCounters = ({ good, neutral, bad }) => {
-  var total = good + neutral + bad;
-  var average = (((good - bad) / total) * 100).toFixed(2);
-  var positive = ((good / total) * 100).toFixed(2);
-  return total === 0 ? (
-    <p>We don't have any statistics yet</p>
-  ) : (
-    <div>
-      <table>
-        <tbody>
-          <tr>
-            <td>Good</td>
-            <td>{good}</td>
-          </tr>
-          <tr>
-            <td>Neutral</td>
-            <td>{neutral}</td>
-          </tr>
-          <tr>
-            <td>Bad</td>
-            <td>{bad}</td>
-          </tr>
-          <tr>
-            <td>All: </td>
-            <td>{total}</td>
-          </tr>
-          <tr>
-            <td>Average: </td>
-            <td>{average} %</td>
-          </tr>
-          <tr>
-            <td>Positive</td>
-            <td>{positive} %</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-const Print = () => {
   return (
     <div>
-      <table>
-        <tr>
-          <td></td>
-        </tr>
-      </table>
+      <h1>{course.name}</h1>
+      <ul>{items}</ul>
+      <p><strong>Total of {suma} exercises</strong></p>
     </div>
   );
 };
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  };
 
-  return (
-    <div>
-      <h1>Give feedback</h1>
-      <ButtonCounter text="good" onClick={() => setGood(good + 1)} />
-      <ButtonCounter text="neutral" onClick={() => setNeutral(neutral + 1)} />
-      <ButtonCounter text="bad" onClick={() => setBad(bad + 1)} />
-      <h1>Statistics</h1>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <AllCounters good={good} neutral={neutral} bad={bad} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 export default App;
